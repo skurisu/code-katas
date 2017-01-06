accba92198e13810e09c0759437ccfe22945e9ac
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-01-05 21:55:48 -0500

    small refactor

--- sum-of-the-first-nth-term-of-series.js
+++ sum-of-the-first-nth-term-of-series.js
@@ -2,7 +2,6 @@ function SeriesSum(n) {
   if(n === 0) return "0.00";
   if(n === 1) return "1.00";
 
-  
   var nums = [];
   var denominator = 1;
   
@@ -17,6 +16,29 @@ function SeriesSum(n) {
   }, 0);
   
   var total = (1 + sum).toFixed(2);
+  
+  return total
+
+}
+
+/* Refactor 1*/
+function SeriesSum(n) {
+  if(n === 0) return "0.00";
+  if(n === 1) return "1.00";
+
+  var nums = [1];
+  var denominator = 1;
+  
+  for(i = 0; i < n - 1; i ++) {
+    denominator += 3;
+    nums.push(denominator);
+  }
+  
+  var sum = nums.reduce(function(a, b) {
+    return a + (1/b)
+  }, 0);
+  
+  var total = sum.toFixed(2);
   
   return total
 

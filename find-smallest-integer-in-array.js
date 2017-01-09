2130d361b69bba661b33b2e06a9536115af1955e
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-01-08 23:29:50 -0500

    smallest integer using reduce

--- find-smallest-integer-in-array.js
+++ find-smallest-integer-in-array.js
@@ -3,3 +3,15 @@ class SmallestIntegerFinder {
     return Math.min.apply(null,args);
   }
 }
+
+/* practice with reduce */
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.reduce(function(prev,current){
+      if(prev < current) {
+        return prev;
+      }
+      return current;
+    }, Infinity)
+  }
+}

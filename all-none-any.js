cebfb478c2f4b35df7f1988ec4504cad1f2ecd7d
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-01-12 22:44:03 -0500

    all-none-any refactor

--- all-none-any.js
+++ all-none-any.js
@@ -1,11 +1,11 @@
 Array.prototype.all = function (p) {
   //this : array
   //p : function
-  var evaluated = this.map(function(current){
+  var evaluated = this.map(function (current) {
     return p(current);
   });
   
-  var filtered = evaluated.filter(function(value){
+  var filtered = evaluated.filter(function (value) {
     return value; 
   });
   
@@ -18,11 +18,11 @@ Array.prototype.all = function (p) {
 
 Array.prototype.none = function (p) {
   // TODO: Implement this function
-  var evaluated = this.map(function(current){
+  var evaluated = this.map(function (current) {
     return p(current);
   });
   
-  var filtered = evaluated.filter(function(value){
+  var filtered = evaluated.filter(function (value) {
     return value; 
   });
   
@@ -34,12 +34,46 @@ Array.prototype.none = function (p) {
 
 Array.prototype.any = function (p) {
   // TODO: Implement this function
-  var evaluated = this.map(function(current){
+  var evaluated = this.map(function (current) {
     return p(current);
   });
   
-  var filtered = evaluated.filter(function(value){
+  var filtered = evaluated.filter(function (value) {
     return value; 
+  });
+  
+  if(filtered.length > 0) {
+    return true;
+  }
+  return false;
+};
+
+/* Refactor 1*/
+Array.prototype.all = function (p) {
+  var filtered = this.filter(function (value) {
+    return p(value);
+  });
+  
+  if(evaluated.length === filtered.length) {
+    return true;
+  }
+  return false;
+};
+
+Array.prototype.none = function (p) {
+  var filtered = this.filter(function (value) {
+    return p(value);
+  });
+  
+  if(filtered.length === 0) {
+    return true;
+  }
+  return false;
+};
+
+Array.prototype.any = function (p) {
+  var filtered = this.filter(function (value) {
+    return p(value);
   });
   
   if(filtered.length > 0) {

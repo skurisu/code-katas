a1ab11427164dbb2732fd378ebdf0810d356fcdf
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-04-18 10:30:47 -0400

    sorting the odd way refactor

--- sorting-the-odd-way.js
+++ sorting-the-odd-way.js
@@ -19,3 +19,10 @@ function sortItOut(array){
 
   return (finalArr);
 }
+
+
+/* Refactor */
+function sortItOut(array){
+  return array.filter(a => Math.floor(a) % 2 !== 0).sort((a,b) => a - b)
+  .concat(array.filter(a => Math.floor(a) % 2 === 0).sort((a,b) => b - a));
+}
